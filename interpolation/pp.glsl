dc9a4b4e13ed672eff31cb8b9ad8a9260017d395
Tyrell Sassen <tyrells@gmail.com>
2020-05-10 18:19:25 -0700

    Added additional CRT shaders
    The additional shaders ported/tweaked by Delfino Furioso have been added
    to the repo. I edited most of these new shaders to remove unnecessary
    uniforms / variables. I also added some additional code to ensure the
    correct filtering method is used, where the shader does not take this
    into account automatically.
    
    The original shader sources can be found here:
    https://www.vogons.org/viewtopic.php?f=32&t=72697

--- interpolation/pp.glsl
+++ interpolation/pp.glsl
@@ -160,11 +160,71 @@ precision mediump float;
 uniform sampler2D rubyTexture;
 uniform vec2 rubyTextureSize;
 
+/*
+	The following code allows the shader to override any texture filtering
+	configured in DOSBox. if 'output' is set to 'opengl', bilinear filtering
+	will be enabled and OPENGLNB will not be defined, if 'output' is set to
+	'openglnb', nearest neighbour filtering will be enabled and OPENGLNB will
+	be defined.
+
+	If you wish to use the default filtering method that is currently enabled
+	in DOSBox, use COMPAT_TEXTURE to lookup a texel from the input texture.
+
+	If you wish to force nearest-neighbor interpolation use NN_TEXTURE.
+
+	If you wish to force bilinear interpolation use BL_TEXTURE.
+
+	If DOSBox is configured to use the filtering method that is being forced,
+	the default	hardware implementation will be used, otherwise the custom
+	implementations below will be used instead.
+
+	These custom implemenations rely on the `rubyTextureSize` uniform variable.
+	The code could calculate the texture size from the sampler using the
+	textureSize() GLSL function, but this would require a minimum of GLSL
+	version 130, which may prevent the shader from working on older systems.
+*/
+
+#if defined(OPENGLNB)
+#define NN_TEXTURE COMPAT_TEXTURE
+#define BL_TEXTURE blTexture
+vec4 blTexture(in sampler2D sampler, in vec2 uv)
+{
+	// subtract 0.5 here and add it again after the floor to centre the texel
+	vec2 texCoord = uv * rubyTextureSize - vec2(0.5);
+	vec2 s0t0 = floor(texCoord) + vec2(0.5);
+	vec2 s0t1 = s0t0 + vec2(0.0, 1.0);
+	vec2 s1t0 = s0t0 + vec2(1.0, 0.0);
+	vec2 s1t1 = s0t0 + vec2(1.0);
+
+	vec2 invTexSize = 1.0 / rubyTextureSize;
+	vec4 c_s0t0 = COMPAT_TEXTURE(sampler, s0t0 * invTexSize);
+	vec4 c_s0t1 = COMPAT_TEXTURE(sampler, s0t1 * invTexSize);
+	vec4 c_s1t0 = COMPAT_TEXTURE(sampler, s1t0 * invTexSize);
+	vec4 c_s1t1 = COMPAT_TEXTURE(sampler, s1t1 * invTexSize);
+
+	vec2 weight = fract(texCoord);
+
+	vec4 c0 = c_s0t0 + (c_s1t0 - c_s0t0) * weight.x;
+	vec4 c1 = c_s0t1 + (c_s1t1 - c_s0t1) * weight.x;
+
+	return (c0 + (c1 - c0) * weight.y);
+}
+#else
+#define BL_TEXTURE COMPAT_TEXTURE
+#define NN_TEXTURE nnTexture
+vec4 nnTexture(in sampler2D sampler, in vec2 uv)
+{
+	vec2 texCoord = floor(uv * rubyTextureSize) + vec2(0.5);
+	vec2 invTexSize = 1.0 / rubyTextureSize;
+	return COMPAT_TEXTURE(sampler, texCoord * invTexSize);
+}
+#endif
+
 COMPAT_VARYING vec2 outCoord;
 
 void main()
 {
-	vec4 outColor = COMPAT_TEXTURE(rubyTexture, outCoord * rubyInputSize / rubyTextureSize);
+	vec4 outColor = NN_TEXTURE(rubyTexture, outCoord * rubyInputSize / rubyTextureSize);
 	if ( outCoord.x >= 0.0 && outCoord.x <= 1.0 && outCoord.y >= 0.0 && outCoord.y <= 1.0)
 		FragColor = outColor;
 	else
